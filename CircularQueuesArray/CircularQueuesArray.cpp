511a0f0db1dfb8577fffbdbdd146931d9ad6e385
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-05-30 14:39:46 +0700

    Tambahkan konstruktor dan masukkan metode ke kelas Antrian

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -7,5 +7,16 @@ private:
 	int FRONT, REAR;
 	int queue_array[max];
 
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
 
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+	}
 };
