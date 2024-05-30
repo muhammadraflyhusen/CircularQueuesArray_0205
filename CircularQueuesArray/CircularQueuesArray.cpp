fbd1cdee6dd9938008e4bc6f9ed19f99cc75b5d1
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-05-30 15:12:42 +0700

    antrian hanya memiliki 1 elemen

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -48,5 +48,10 @@ public:
 			return;
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
 	}
 };
