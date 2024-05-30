3f5567bb11ec1c8f346e9a0dfce67bd0fc6a3bff
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-05-30 15:27:03 +0700

    Tambahkan logika iterasi untuk elemen pencetakan saat FRONT membungkusnya

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -81,7 +81,11 @@ public:
             }
             cout << endl;
         }
-        
-        
+		else {
+			while (FRONT_Position <= max - 1) {
+				cout << queue_array[FRONT_Position] << "   ";
+				FRONT_Position++;
+			}
+		}
     }
 };
